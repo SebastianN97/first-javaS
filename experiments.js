7029f2132ac013f059a8985de85fa057da2c90d9
SebastianN97 <sebbe132132@gmail.com>
2020-08-03 20:00:47 +0200

    taking notes of variables JS

--- experiments.js
+++ experiments.js
@@ -1,5 +1,16 @@
 
 
+let apples = 4
+let weekday = "Thursday"
+
+console.log("On " + weekday + ", I ate " + apples + " apples")
+
+
+const apples = 5
+const weekday = "Thursday"
+
+const APPLES = 5
+const WEEKDAY = "Thursday"
 
 
 
