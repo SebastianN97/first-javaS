b180b34766559e29e41efbf02527f49d67e8667d
SebastianN97 <sebbe132132@gmail.com>
2020-08-04 20:18:42 +0200

    starting with Logic js

---
+++ Operators.js
@@ -0,0 +1,32 @@
+
+// Addition... Same goes for sub, div and multiplication
+
+let blueCars = 4
+let redCars = 5
+
+let totalCars = blueCars + redCars
+
+console.log("I have " + totalCars + " cars!")
+
+
+let tapasCount = 4
+tapasCount++                // adds in 1 extra
+
+console.log(`I have ${tapasCount} tapas`)
+
+
+let bookCount = 10
+bookCount += 20             // adds in 20 books
+
+console.log("I have " + bookCount + " books!")
+
+
+// Modules
+
+let people = 20
+let peoplePerCar = 6
+
+let peopleInLastCar = people % peoplePerCar     // Gives us the rest of the number
+
+console.log("The last car will have " + peopleInLastCar + " people in it!")
+
---
+++ PrimitiveData.js
@@ -0,0 +1,44 @@
+// Numbers
+
+let cars = 7;
+console.log(cars);
+
+typeof(cars);               // tells what class cars is
+
+
+// Strings
+
+let city = "Gothenburg";
+console.log("The second largest city in Sweden is " + city + ".");
+
+
+// Template Literals
+
+console.log('Makes us be
+ able to store multiple
+ lines'                         
+);                          // Doesn't seem to work..
+
+
+// String Interpolation
+
+let favorite = "Pinchos";
+console.log("my favorite family restaurant is ${favorite}")
+
+
+// Booleans 
+
+var javaScriptIsInterpreted = true
+var javaScriptIsDifficultToLearn = false
+
+
+// Null
+
+var nothing = null;
+
+
+
+
+
+
+
